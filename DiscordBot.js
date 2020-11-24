4be7f007af76681c3e44352366cfbe12822605ca
Til7701 <tilman.holube@t-online.de>
2020-11-24 19:24:50 +0100

    Completed addChannel Command

--- DiscordBot.js
+++ DiscordBot.js
@@ -19,26 +19,29 @@ class DiscordBot {
             let channelFilePath = "config/discordchannels.json";
 
             if (msg.content === prefix + 'ping') {
-            msg.reply('Pong!');
+                msg.reply('Pong!');
             }
 
             if (msg.content.startsWith(prefix)) {
                 switch(true) {
                     case msg.content.startsWith(prefix + "refresh"):
                         console.log("Testcycle started manually!")
+                        msg.reply("Testcycle started manually!");
                         testCycle();
                         break;
                     case msg.content.startsWith(prefix + "addChannel"):
-                        let content = msg.content.split(" ");
+                        let content = msg.content.split(" "); //split in arguments
                         if (content.length == 3) {
+                            //Get the Channel ID if it should be the one the message was sent in.
                             if (content[2] == "this") {
                                 content[2] = msg.channel.id;
                             }
                             
+                            //Adding the Channel to JSON File
                             const channelFile = JSON.parse(fs.readFileSync(channelFilePath));
                             channelFile['channels'].push({"name":content[1],"id":content[2]});
                             console.log(channelFile);
-                            fs.writeFileSync(channelFile, JSON.stringify(channelFilePath, false, 2))
+                            fs.writeFileSync(channelFilePath, JSON.stringify(channelFile, false, 2))
 
                             msg.reply("Added Channel " + content[2] + " as " + content[1]);
                             console.log("Added Channel " + content[2] + " as " + content[1]);
